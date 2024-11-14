03eb17e1e1b7789145dccf04a6a7b70e01f7fc18
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-11-14 10:39:32 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,3 @@
 function gazo688() {
-    document.getElementById('who688').innerHTML = '<img src="./image/naku.png" alt="オン">';
+    document.getElementById('who688').innerHTML = '<img src="./image/立つ.png" alt="オン">';
 }
