13e259cabf2d0dd6b6944436001e8fcab5972103
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-11-14 10:40:22 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,6 @@
 function imgchange1() {
-    document.getElementById('logo').src = './image/jousaikyara.jpg';
+    document.getElementById('logo').src = './image/城西キャラクター.jpg';
 }
 function imgchange2() {
-    document.getElementById('logo').src = './image/josai.jpg';
+    document.getElementById('logo').src = './image/城西ロゴ.jpg';
 }
