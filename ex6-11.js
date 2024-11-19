ca49f76f868c771f09a8341fe9945b9499ab4d41
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-11-19 10:30:39 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
 function imagchange3() {document.getElementById('logo').src = ' ./image/城西キャラクター.jpg';}
-function imagchange4() {document.getElementById('logo').src = ' ./image/josailogo.jpg';}
+function imagchange4() {document.getElementById('logo').src = ' ./image/城西ロゴ.jpg';}
