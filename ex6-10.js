c3626cffe00385d2832e4794151e2c0d08242335
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-11-28 11:07:07 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,6 @@
 function imgchange1() {
-    document.getElementById('mark').src = './image/立つ.pmg';
+    document.getElementById('mark').src = './image/立つ.png';
 }
 function imgchange2() {
-    document.getElementById('mark').src = './image/体育座り.pmg';
+    document.getElementById('mark').src = './image/体育座り.png';
 }
