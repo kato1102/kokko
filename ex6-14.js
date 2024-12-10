7fbaad1323cdf09da6a75ee5c810a92f8390f11a
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-12-10 10:01:31 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
 function imgchange5(){document.getElementById('minion').src='./image/R.jpg';}
-function mes2(){document.getElementById('minion').src='./image/ミニオン2.jpg';}
+function mes2(){document.getElementById('minion').src='./image/minion.jpg';}
