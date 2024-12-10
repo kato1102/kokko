6a6e99adb0b0a61b2d08d2edd14532f67a5944e8
kato1102 <167833150+kato1102@users.noreply.github.com>
2024-12-10 10:42:37 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,5 +1,4 @@
-function add(){
-  var x =document.getElementByld('input1').value;
-  var y =document.getElementByld('input2').value;
-  alert(x+y);
-}
+function add(){var x =document.getElementByld('input1').value;
+               var y =document.getElementByld('input2').value;
+               alert(x+y);
+               }
