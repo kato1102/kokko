00d0026a5b910207130c44aafc0fdcef858eccf2
kato1102 <167833150+kato1102@users.noreply.github.com>
2025-01-07 10:16:01 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,2 +1,2 @@
-function nazo75(){if(document.getElementById('anzwer75').value=='夢'){alert('あたり!');}
-                  else{alert('はずれ');}}
+function nazo75(){if(document.getElementById('answer75').value=='夢'){alert('あたり!');}
+                else{alert('はずれ');}}
